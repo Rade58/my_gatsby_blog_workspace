ddf3251411a2051b6bb18fe7da6379c766ca79c8
Rade <bajic.rade2@gmail.com>
2020-04-16 21:54:35 +0200

    redefined graphql layer, helmet is used for head section metadata and seo stuff

--- packages/gatsby-theme-raedal/src/seo/seo.tsx
+++ packages/gatsby-theme-raedal/src/seo/seo.tsx
@@ -1,13 +1,15 @@
 import React, { FunctionComponent } from "react";
 import { Helmet } from "react-helmet";
 
+// OVAJ INTERFACE KORISTIM U VISE KOMPONENTI TAKO, KADA GA PROMENIS, UTICACE I
+// NA SLEDECE FAJLOVE TAKODJE
+// <   packages/gatsby-theme-raedal/src/templates/blog-post-template.tsx  >
+// <   packages/gatsby-theme-raedal/src/components/blog-post.tsx   >
 export interface SeoI {
   title: string;
   lang: string;
   description: string;
   themeColor: string;
-
-  drvece: number;
 }
 
 const Seo: FunctionComponent<SeoI> = ({
