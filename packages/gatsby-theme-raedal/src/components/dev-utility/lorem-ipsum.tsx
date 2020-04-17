9c8f4c967a2bebe6928591135ebdbd416c2820b1
Rade <bajic.rade2@gmail.com>
2020-04-17 16:19:52 +0200

    menaging state with reducer

---
+++ packages/gatsby-theme-raedal/src/components/dev-utility/lorem-ipsum.tsx
@@ -0,0 +1,80 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
+import { FunctionComponent } from "react";
+import { css } from "@emotion/core";
+
+const LoremIpsum: FunctionComponent = () => (
+  <div
+    className="lorem"
+    css={css`
+      width: 50vw;
+      border: blanchedalmond solid 2px;
+      margin: 0 auto;
+    `}
+  >
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a magna
+      luctus nisl sollicitudin tempor ut vitae nulla. Ut pretium pharetra
+      lacinia. Etiam ultrices sodales velit vitae fermentum. Sed faucibus
+      laoreet viverra. Donec porttitor neque quis interdum tincidunt. Vivamus
+      congue consequat ante, vel cursus libero dignissim in. Interdum et
+      malesuada fames ac ante ipsum primis in faucibus. Suspendisse porta purus
+      vel lacus varius, sit amet imperdiet nisl consectetur. Integer porttitor
+      arcu pulvinar diam convallis iaculis nec viverra est. Donec pharetra,
+      magna in pulvinar scelerisque, lacus dolor interdum lorem, sed dictum
+      felis libero nec sem. Maecenas lectus mi, tristique ac tristique lacinia,
+      pellentesque lacinia eros. Aenean ac augue ante. Integer a viverra sapien.
+      Morbi a erat quis nisl pulvinar venenatis quis id felis. Pellentesque non
+      quam tincidunt, cursus dui interdum, sollicitudin eros. Maecenas finibus
+      non nulla non sodales. Etiam metus mauris, interdum ut neque vitae,
+      condimentum pharetra sem. Pellentesque mattis nibh sed tempus ornare.
+      Vestibulum id sapien turpis. Proin iaculis interdum efficitur. Suspendisse
+      eget dictum mi, in tincidunt lectus. Nam pharetra ipsum sit amet semper
+      ultricies. Integer quis lectus lacus. Sed condimentum tempor dignissim.
+      Praesent at molestie lacus. Fusce tristique magna arcu, vitae molestie
+      eros iaculis at. Nullam iaculis nulla mi, in blandit leo commodo non. Nunc
+      sit amet posuere orci. Donec scelerisque tempor diam, ut aliquam tellus
+      cursus eu. Suspendisse volutpat sed odio eget tristique. Morbi sit amet
+      magna ac quam sagittis rhoncus rhoncus eu metus. Morbi volutpat dui eu
+      augue pellentesque, aliquet bibendum massa maximus. Aliquam porttitor
+      massa id metus aliquam hendrerit. Aenean sollicitudin, nibh nec fermentum
+      maximus, enim tellus porta nibh, ac tristique velit turpis quis odio. Duis
+      tempor, est sit amet euismod porttitor, est nunc tempor orci, fermentum
+      finibus ante purus nec leo. Nam sed diam accumsan, finibus lorem nec,
+      ultricies neque. Phasellus luctus, ex et pulvinar venenatis, dolor turpis
+      tincidunt justo, eu mollis nisi ligula in quam. Nullam ac molestie ante.
+      Aenean sem magna, lacinia id feugiat at, porta a ipsum. Vestibulum id
+      rhoncus turpis. Curabitur porta odio ut dolor suscipit, non commodo ex
+      sodales. Cras molestie orci vitae urna consectetur tempor. Sed a
+      vestibulum metus. Nam ut tincidunt enim. Suspendisse vulputate risus nec
+      massa laoreet, nec tempor nunc imperdiet. Sed condimentum diam ac turpis
+      hendrerit, a convallis velit suscipit. Donec eget lacinia sem, id iaculis
+      ante. Donec suscipit leo nec neque sodales finibus. Suspendisse
+      condimentum venenatis dui at dignissim. Phasellus vitae tempus neque.
+      Mauris porta libero in quam accumsan, in malesuada eros sagittis. Proin
+      posuere nibh ut risus rutrum, id lobortis eros dapibus. Donec sed nisi nec
+      lectus tincidunt luctus. Donec imperdiet diam quis euismod maximus. Cras
+      finibus neque sed porta luctus. Morbi dictum tortor nec dolor scelerisque,
+      sit amet dignissim mauris interdum. Maecenas et interdum est. Donec
+      interdum diam et purus gravida, molestie faucibus nisi scelerisque. Duis
+      in ex sapien. In ac tincidunt risus. Nulla eget ex eu nisi aliquet cursus
+      sit amet eget dui. Fusce mattis euismod tortor, viverra cursus massa
+      placerat ut. Maecenas vestibulum sagittis orci, id molestie ante cursus a.
+      Vestibulum cursus iaculis blandit. Donec commodo est quis facilisis
+      faucibus. Duis quis finibus turpis. Proin pellentesque bibendum molestie.
+      Nullam eget nulla tincidunt, luctus ante ac, fringilla nibh. Proin
+      ultricies lacinia massa, a ornare purus eleifend sit amet. Sed scelerisque
+      venenatis mauris non accumsan. Nunc eleifend massa id elit ultricies
+      auctor. Nam tristique quam augue, in fermentum quam convallis dignissim.
+      Ut ultrices enim nec malesuada gravida. Vivamus ac enim diam. Vestibulum
+      vel ipsum sapien. Nam a mollis libero, non tristique neque. Phasellus in
+      rhoncus quam. Curabitur vestibulum lobortis nunc, pretium cursus nulla
+      mattis et. Etiam molestie, est id maximus gravida, metus leo ullamcorper
+      neque, vel sollicitudin tellus neque vel nulla. Duis aliquam euismod
+      lacinia. Praesent eleifend leo non faucibus viverra.
+    </p>
+  </div>
+);
+
+export default LoremIpsum;
--- packages/gatsby-theme-raedal/src/components/layout.tsx
+++ packages/gatsby-theme-raedal/src/components/layout.tsx
@@ -1,39 +1,98 @@
 /** @jsx jsx */
 import { jsx, ThemeProvider } from "theme-ui";
 
-import { Fragment, FunctionComponent } from "react";
+import {
+  Fragment,
+  useState,
+  useReducer,
+  // -->   types
+  FunctionComponent,
+  Reducer,
+  ReducerState,
+  ReducerAction,
+} from "react";
 import { Global, css } from "@emotion/core";
 
 //
 import theme from "../gatsby-plugin-theme-ui/index";
 //
 
-const Layout: FunctionComponent = ({ children }) => (
-  <Fragment>
-    <ThemeProvider theme={theme}>
-      <Global
-        styles={{
-          body: { margin: 0 },
-        }}
-      />
-      <header
-        css={css`
-          border-top: 14px solid purple;
-        `}
-      >
-        <strong>Blog Post Layout</strong>
-      </header>
-      <main>{children}</main>
-      <button
-        sx={{
-          variant: "myButton",
-        }}
-        type="button"
-      >
-        Press me
-      </button>
-    </ThemeProvider>
-  </Fragment>
-);
+// LOREM IPSUM
+import LoremIpsum from "./dev-utility/lorem-ipsum";
+//
+
+// ********  REDUCER STUFF   ********************
+// REDUCER CU KORISTITI ZA STATE KOJI SE NE MANJE FROM PAGE TO PAGE
+
+enum ACTIONS_ENUM {
+  SCROLL_UP = "SCROLL_UP",
+  SCROLL_DOWN = "SCROLL_DOWN",
+}
+
+interface StateI {
+  blah: string;
+}
+
+const reducer: Reducer<StateI, ACTIONS_ENUM> = (state, action) => {
+  let placeholder;
+  return state;
+};
+
+// ************   *************************************
+
+const Layout: FunctionComponent = ({ children }) => {
+  const state = useReducer(reducer, { blah: "" });
+
+  return (
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <Global
+          styles={{
+            body: {
+              margin: "4px",
+              backgroundColor: "olive",
+              paddingTop: "56px",
+            },
+          }}
+        />
+        <header
+          css={css`
+            /* border-top: 14px solid purple; */
+
+            height: 56px;
+            border-bottom: black 2px solid;
+
+            background-color: #ffffff;
+
+            position: fixed;
+            width: 100%;
+            top: 0;
+            left: 0;
+
+            &.scroll-down {
+              top: -52px;
+            }
+          `}
+        >
+          <strong>Blog Post Layout</strong>
+        </header>
+        <main>
+          {children}
+          {/* SAMO TU DA STVORI PROSTOR */}
+          <LoremIpsum />
+          {/* /////////////////////// */}
+        </main>
+        <button
+          sx={{
+            variant: "myButton",
+          }}
+          type="button"
+        >
+          Press me
+        </button>
+      </ThemeProvider>
+    </Fragment>
+  );
+};
 
 export default Layout;
