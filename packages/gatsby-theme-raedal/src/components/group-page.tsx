08dea4b466a27c7869d2d2ea9966c934e45cb3e4
Rade <bajic.rade2@gmail.com>
2020-05-04 01:56:30 +0200

    created some template and component files

---
+++ packages/gatsby-theme-raedal/src/components/group-page.tsx
@@ -0,0 +1,6 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
+// dev utility
+import StringifiedProps from "./dev-utility/StringifyProps";
+//
--- packages/gatsby-theme-raedal/src/templates/group-page-template.tsx
+++ packages/gatsby-theme-raedal/src/templates/group-page-template.tsx
@@ -2,3 +2,4 @@ import React, { FunctionComponent } from "react";
 import { graphql } from "gatsby";
 
 // UVESCU KOMPONENTU, KADA JE NAPRAVIM
+// A SADA CU NAPRAVITI QUERY
