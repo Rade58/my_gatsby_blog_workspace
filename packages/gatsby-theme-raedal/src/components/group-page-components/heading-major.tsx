ac70e623f0258119c26b342be39118d4d8182cbe
rade <bajic.rade2@gmail.com>
2020-05-14 19:50:05 +0200

    sstyling

--- packages/gatsby-theme-raedal/src/components/group-page-components/heading-major.tsx
+++ packages/gatsby-theme-raedal/src/components/group-page-components/heading-major.tsx
@@ -12,7 +12,7 @@ const HeadingMajor: FunctionComponent = () => {
 
   const { groupPage } = useContext(groupPageContext);
 
-  const { name } = groupPage; // NAME CE BITI ONO STA JE U TITLE-U
+  const { name, groupColor } = groupPage; // NAME CE BITI ONO STA JE U TITLE-U
   // DOBRA IDEJA JE DA OVDE STAVIS I EMOJI
   // ODNOSNO SVAKI PAGE TREBA DA IMA KARAKTERISTICAN EMOJI
 
@@ -23,7 +23,25 @@ const HeadingMajor: FunctionComponent = () => {
         display: flex;
       `}
     >
-      <h1> {name}</h1>
+      <h1
+        css={css`
+          margin-left: auto;
+          margin-right: auto;
+
+          & span {
+            &.group-name-span {
+              text-shadow: 1px 1px 1px ${groupColor};
+              color: blanchedalmond;
+            }
+          }
+        `}
+      >
+        All <span className="group-name-span">{name}</span> tutorials
+        &nbsp;&nbsp;{" "}
+        <span role="img" aria-label="img">
+          ⬇️
+        </span>
+      </h1>
     </div>
   );
 };
