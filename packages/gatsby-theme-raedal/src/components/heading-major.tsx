52088f743edc247f0e2ee2c6f65099f024e4def9
rade <bajic.rade2@gmail.com>
2020-05-22 17:58:02 +0200

    displayed linked icon

--- packages/gatsby-theme-raedal/src/components/heading-major.tsx
+++ packages/gatsby-theme-raedal/src/components/heading-major.tsx
@@ -10,14 +10,20 @@ import { $_useBlogPostReducerState } from "../context_n_reducers/context_n_reduc
 
 const HeadingMajor: FunctionComponent = () => {
   const { blogPostContext } = $_useBlogPostReducerState;
-  const { createdAt, isUpdated, updated, seo } = useContext(blogPostContext);
+  const { createdAt, isUpdated, updated, seo, groupPage } = useContext(
+    blogPostContext
+  );
   const { title, themeColor } = seo;
+  const { icon, path } = groupPage;
 
   return (
     <section className="major-heading">
       <h1>{title}</h1>
       <h3>{createdAt}</h3>
       {isUpdated ? <h3>{updated}</h3> : null}
+      <Link to={path}>
+        <img src={`data:image/svg+xml;base64,${icon}`} alt="group-icon" />
+      </Link>
       <div
         css={css`
           border: ${themeColor} solid 12px;
