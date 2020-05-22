b6afc9858efaf1f2579cc216f1433e393b1efcfa
rade <bajic.rade2@gmail.com>
2020-05-22 19:27:23 +0200

    styling heading

--- packages/gatsby-theme-raedal/src/components/heading-major.tsx
+++ packages/gatsby-theme-raedal/src/components/heading-major.tsx
@@ -3,7 +3,7 @@ import { jsx } from "theme-ui";
 import { useContext, FunctionComponent } from "react";
 import { css } from "@emotion/core";
 import { Link } from "gatsby";
-import styled from "@emotion/styled";
+// import styled from "@emotion/styled";
 //
 import { $_useBlogPostReducerState } from "../context_n_reducers/context_n_reducer_blog_post";
 //
@@ -17,16 +17,86 @@ const HeadingMajor: FunctionComponent = () => {
   const { icon, path } = groupPage;
 
   return (
-    <section className="major-heading">
-      <h1>{title}</h1>
-      <h3>{createdAt}</h3>
-      {isUpdated ? <h3>{updated}</h3> : null}
-      <Link to={path}>
-        <img src={`data:image/svg+xml;base64,${icon}`} alt="group-icon" />
-      </Link>
+    <section
+      className="major-heading"
+      css={css`
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+        align-items: center;
+        align-content: stretch;
+
+        & .headingz {
+          display: flex;
+          flex-direction: row;
+          width: 100%;
+          border: olive solid 1px;
+
+          & h1.h-title {
+            font-weight: 400;
+            margin: 20px auto;
+            font-size: 55px;
+            border: pink solid 1px;
+          }
+
+          & a {
+            height: min-content;
+            margin-right: 0.2rem;
+            margin-left: auto;
+            margin-top: auto;
+            margin-bottom: auto;
+            transition-property: transform;
+            transition-duration: 0.4s;
+
+            &:hover {
+              transform: scale(1.2);
+            }
+          }
+
+          & img {
+            height: 2rem;
+          }
+        }
+
+        & div.times {
+          display: flex;
+          flex-direction: row;
+          width: 100%;
+
+          & h4.created {
+            border: crimson solid 1px;
+            margin-right: auto;
+            justify-self: flex-start;
+          }
+
+          & h4.updated {
+            border: tomato solid 1px;
+          }
+        }
+      `}
+    >
+      <div className="headingz">
+        <Link to={path}>
+          <img src={`data:image/svg+xml;base64,${icon}`} alt="group-icon" />
+        </Link>
+        <h1 className="h-title">{title}</h1>
+      </div>
+
+      <div className="times">
+        <h4 className="created">
+          <span>Published :</span> {createdAt}
+        </h4>
+        {isUpdated ? (
+          <h4 className="updated">
+            <span>updated: </span> {updated}
+          </h4>
+        ) : null}
+      </div>
       <div
         css={css`
-          border: ${themeColor} solid 12px;
+          border-top: ${themeColor} solid 2px;
+          width: 80%;
+          height: 0px;
         `}
       />
     </section>
