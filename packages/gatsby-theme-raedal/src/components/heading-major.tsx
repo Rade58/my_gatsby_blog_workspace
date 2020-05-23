0d61bf67daaf5ca9b161e06508ba3adc08a271d6
rade <bajic.rade2@gmail.com>
2020-05-23 18:33:23 +0200

    line markers

--- packages/gatsby-theme-raedal/src/components/heading-major.tsx
+++ packages/gatsby-theme-raedal/src/components/heading-major.tsx
@@ -35,27 +35,10 @@ const HeadingMajor: FunctionComponent = () => {
           & h1.h-title {
             font-weight: 400;
             margin-right: auto;
+            margin-left: auto;
             font-size: 55px;
             border: pink solid 1px;
             text-align: center;
-          }
-
-          & a {
-            height: min-content;
-            margin-right: 0.2rem;
-            margin-left: auto;
-            margin-top: auto;
-            margin-bottom: auto;
-            transition-property: transform;
-            transition-duration: 0.4s;
-
-            &:hover {
-              transform: scale(1.2);
-            }
-          }
-
-          & img {
-            height: 2rem;
           }
         }
 
@@ -64,41 +47,73 @@ const HeadingMajor: FunctionComponent = () => {
           flex-direction: row;
           flex-wrap: wrap;
           width: 100%;
+
+          & .mark-line {
+            width: 10%;
+            border-top: ${themeColor} solid 1px;
+            align-self: center;
+
+            @media screen and (max-width: 368px) {
+              display: none;
+            }
+          }
+
           /* ------------------------------------------ */
           /* @media screen and (min-width: 918px) {
             h4.updated {
               width: fit-content;
             }
-
+            
             h4.created {
               width: fit-content;
             }
           }
-
+          
           @media screen and (min-width: 1100px) {
             h4.updated {
               width: max-content;
             }
-
+            
             h4.created {
               width: max-content;
             }
           } */
           /* ------------------------------------- */
+          & a {
+            border: pink solid 4px;
+            height: min-content;
+            width: min-content;
+            justify-self: center;
+            /* margin-right: 0.2rem; */
+            /* margin-right: auto; */
+            /* margin-left: auto;  */
+            margin-top: auto;
+            margin-bottom: auto;
+            transition-property: transform;
+            transition-duration: 0.4s;
+
+            & img {
+              height: 2rem;
+            }
+
+            &:hover {
+              transform: scale(1.2);
+            }
+          }
 
           & time {
             color: #e4cdc3;
           }
 
           & h4.created {
-            border: crimson solid 0px;
+            border: crimson solid 1px;
             margin-right: auto;
             margin-left: 1em;
             /* justify-self: flex-start; */
             width: min-content;
             font-weight: 400;
 
-            @media screen and (min-width: 500px) {
+            @media screen and (min-width: 541px) {
               width: max-content;
             }
 
@@ -112,12 +127,13 @@ const HeadingMajor: FunctionComponent = () => {
           }
 
           & h4.updated {
-            border: tomato solid 0px;
+            border: tomato solid 1px;
             margin-right: 1em;
+            margin-left: auto;
             width: min-content;
             font-weight: 400;
 
-            @media screen and (min-width: 500px) {
+            @media screen and (min-width: 541px) {
               width: max-content;
             }
 
@@ -133,9 +149,6 @@ const HeadingMajor: FunctionComponent = () => {
       `}
     >
       <div className="headingz">
-        <Link to={path}>
-          <img src={`data:image/svg+xml;base64,${icon}`} alt="group-icon" />
-        </Link>
         <h1 className="h-title">{title}</h1>
       </div>
 
@@ -143,6 +156,11 @@ const HeadingMajor: FunctionComponent = () => {
         <h4 className="created">
           <span className="pubspan">Published</span>:<time> {createdAt}</time>
         </h4>
+        <div className="mark-line" />
+        <Link to={path}>
+          <img src={`data:image/svg+xml;base64,${icon}`} alt="group-icon" />
+        </Link>
+        <div className="mark-line" />
         {isUpdated ? (
           <h4 className="updated">
             <span className="upspan">updated</span>:<time> {updated}</time>
