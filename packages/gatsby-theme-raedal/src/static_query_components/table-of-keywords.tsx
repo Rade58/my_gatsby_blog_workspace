c6d1d1e0c7cc3fd3402e93581df215ba60b0e1bb
rade <bajic.rade2@gmail.com>
2020-05-29 20:40:13 +0200

    table of keywords

--- packages/gatsby-theme-raedal/src/static_query_components/table-of-keywords.tsx
+++ packages/gatsby-theme-raedal/src/static_query_components/table-of-keywords.tsx
@@ -1,7 +1,24 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { graphql, useStaticQuery } from "gatsby";
+import { FunctionComponent } from "react";
 
-/* interface KeywordData {
+import KeywordModal from "../components/keyword-modal";
 
-} */
+const TableOfKeywords: FunctionComponent = () => {
+  const data = useStaticQuery(graphql`
+    query groupPagesKeywords {
+      groups: allGroupPage {
+        nodes {
+          icon
+          name
+          path
+        }
+      }
+    }
+  `);
+
+  return <KeywordModal keywords={data.groups.nodes} />;
+};
+
+export default TableOfKeywords;
