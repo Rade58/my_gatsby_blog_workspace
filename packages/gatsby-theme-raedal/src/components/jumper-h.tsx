b5c3926541cfc35f730eaab90a806f2f8e3f44eb
rade <bajic.rade2@gmail.com>
2020-05-30 20:41:19 +0200

    jumper isnt mounted if theres no headers

--- packages/gatsby-theme-raedal/src/components/jumper-h.tsx
+++ packages/gatsby-theme-raedal/src/components/jumper-h.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { css } from "@emotion/core";
-import { useContext, FunctionComponent } from "react";
+import { useContext, Fragment, FunctionComponent } from "react";
 import Octicon, { getIconByName } from "@primer/octicons-react";
 import { $_useBlogPostReducerState } from "../context_n_reducers/context_n_reducer_blog_post";
 
@@ -14,15 +14,35 @@ const JumpButtons: FunctionComponent = () => {
   const arrowUp = getIconByName("arrow-up");
 
   return (
-    <aside className="jumper-btns">
-      <div className="h-changer">
-        <Octicon icon={triangleUp} />
-        <Octicon icon={triangleDown} />
-      </div>
-      <div className="scroll-to-top">
-        <Octicon icon={arrowUp} />
-      </div>
-    </aside>
+    <Fragment>
+      {!headings.length ? null : (
+        <aside
+          className="jumper-cont"
+          css={css`
+            .show-me {
+              display: inline-block;
+            }
+
+            .hide-me {
+              display: none;
+            }
+
+            @media screen and (max-width: 918px) {
+              display: none;
+            }
+          `}
+        >
+          <div className="h-changer">
+            <Octicon icon={triangleUp} />
+            <Octicon icon={triangleDown} />
+            <optgroup></optgroup>
+          </div>
+          <div className="scroll-to-top">
+            <Octicon icon={arrowUp} />
+          </div>
+        </aside>
+      )}
+    </Fragment>
   );
 };
 
