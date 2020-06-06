6190d204f91e6b05ed36d354634fa898d797aabe
rade <bajic.rade2@gmail.com>
2020-06-06 19:43:55 +0200

    planning

--- packages/gatsby-theme-raedal/src/components/author-page-components/layout-author.tsx
+++ packages/gatsby-theme-raedal/src/components/author-page-components/layout-author.tsx
@@ -1,9 +1,10 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { jsx, ThemeProvider } from "theme-ui";
 import { css, Global } from "@emotion/core";
 import { Fragment, FunctionComponent } from "react";
 import AuthorSeo from "../../seo/author-seo";
 import { additionalStyles } from "../../common-styles";
+import theme from "../../gatsby-plugin-theme-ui/index";
 
 const LayoutAuthor: FunctionComponent = () => {
   const { bodyBackgroundColor } = additionalStyles;
@@ -11,16 +12,19 @@ const LayoutAuthor: FunctionComponent = () => {
   return (
     <Fragment>
       <AuthorSeo />
-      <Global
-        styles={{
-          body: {
-            backgroundColor: bodyBackgroundColor,
-            fontFamily:
-              "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-          },
-        }}
-      />
-      <div>Something</div>
+      <ThemeProvider theme={theme}>
+        <Global
+          styles={{
+            body: {
+              backgroundColor: bodyBackgroundColor,
+              fontFamily:
+                "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+            },
+          }}
+        />
+        <div>Something</div>
+        {/* MESTO ZA HEADER I MAIN */}
+      </ThemeProvider>
     </Fragment>
   );
 };
