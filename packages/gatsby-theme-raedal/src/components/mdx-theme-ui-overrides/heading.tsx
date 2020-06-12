8630d5af117ae3ce013993215589f58ee99dd2f8
rade <bajic.rade2@gmail.com>
2020-06-12 16:57:14 +0200

    intersection observer fixed (set state function added as a dependancy inside useEffect INSIDE HEADING COMPONENT)

--- packages/gatsby-theme-raedal/src/components/mdx-theme-ui-overrides/heading.tsx
+++ packages/gatsby-theme-raedal/src/components/mdx-theme-ui-overrides/heading.tsx
@@ -62,7 +62,7 @@ const giveHeading: (Tag: HeadingsI) => FunctionComponent<{ id: string }> = (
         (entries, observer) => {
           // console.log(entries[0].target.id);
 
-          console.log({ entries, observer });
+          // console.log({ entries, observer });
           // console.log(observer.thresholds);
           // console.log(entries[0].rootBounds);
 
