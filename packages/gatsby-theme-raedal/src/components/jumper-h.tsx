149fa621de1a84982dd8f1a85c69176069ad89b4
rade <bajic.rade2@gmail.com>
2020-06-16 19:31:39 +0200

    redefining box shadow

--- packages/gatsby-theme-raedal/src/components/jumper-h.tsx
+++ packages/gatsby-theme-raedal/src/components/jumper-h.tsx
@@ -239,14 +239,27 @@ const JumperButtons: FunctionComponent<JumperPropsI> = ({ mainReference }) => {
           background-color: #20282e88;
 
           border-radius: 16px;
-          box-shadow:
+
+          /* box-shadow:
           0 2.8px 2.2px rgba(0, 0, 0, 0.02),
           0 6.7px 5.3px rgba(0, 0, 0, 0.028),
           0 12.5px 10px rgba(0, 0, 0, 0.035),
           0 22.3px 17.9px rgba(0, 0, 0, 0.042),
           0 41.8px 33.4px rgba(0, 0, 0, 0.05),
           0 100px 80px rgba(0, 0, 0, 0.07)
+          ; */
+
+          box-shadow:
+          0 -1.3px 6.2px rgba(0, 0, 0, 0.093),
+          0 -0.9px 14px rgba(0, 0, 0, 0.134),
+          0 2.2px 26.8px rgba(0, 0, 0, 0.166),
+          0 9px 52.3px rgba(0, 0, 0, 0.194),
+          0 21.9px 64.7px rgba(0, 0, 0, 0.226),
+          0 46.8px 73.1px rgba(0, 0, 0, 0.267),
+          0 100px 80px rgba(0, 0, 0, 0.36)
           ;
+
+
 
 
           padding-top: 1vh;
