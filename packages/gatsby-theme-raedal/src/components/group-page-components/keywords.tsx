649e4a4b769f065832d308f0221ed63e604c0f17
rade <bajic.rade2@gmail.com>
2020-07-09 16:49:16 +0200

    styling group page

--- packages/gatsby-theme-raedal/src/components/group-page-components/keywords.tsx
+++ packages/gatsby-theme-raedal/src/components/group-page-components/keywords.tsx
@@ -12,20 +12,53 @@ const Keywords: FunctionComponent = () => {
 
   const { groupPage } = useContext(groupPageContext);
 
-  const { allBlogKeywords } = groupPage;
+  const { allBlogKeywords, groupColor, keywordTextColor } = groupPage;
 
   return (
     <section
       className="keywords"
       css={css`
         display: flex;
-        border: pink solid 1px;
+        border: pink solid 0px;
         padding: 6px;
+
+        & div.separator {
+          display: block;
+          width: 100%;
+          margin-right: 12%;
+          margin-left: 12%;
+          margin-top: 1vh;
+          margin-bottom: 1vh;
+          height: 1px;
+          /* background-color: crimson; */
+
+          &.separator-1 {
+            background-image: linear-gradient(
+              90deg,
+              rgba(158, 90, 106, 1) 18%,
+              rgba(83, 139, 148, 0.16290266106442575) 80%
+            );
+          }
+
+          &.separator-2 {
+            background-image: linear-gradient(
+              90deg,
+              rgba(83, 139, 148, 0.16290266106442575) 18%,
+              rgba(158, 90, 106, 1) 80%
+            );
+          }
+        }
+        & a:nth-of-type(1) {
+          margin-left: 10%;
+        }
       `}
     >
+      {/* ---separator--- */}
+      <div className="separator separator-1" />
       {allBlogKeywords.map((member) => (
         <Keyword {...member} key={`${member.keyword}-${member.path}`} />
       ))}
+      <div className="separator separator-2" />
     </section>
   );
 };
